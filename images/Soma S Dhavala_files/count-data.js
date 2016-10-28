3251d49b39b8e5d86196d018ade0bef55215f72f
dhavala <soma.dhavala@gmail.com>
2016-10-28 13:45:46 +0530

    activate share links

---
+++ images/Soma S Dhavala_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"2016-05-test-markup","comments":0},{"id":"2014-07-lorem-ipsum-dolor-sit-amet","comments":1},{"id":"2014-08-jekyll-clean-theme","comments":0}]});
+}
